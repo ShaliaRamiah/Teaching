7dc08effa6b4d2984ad5ee0cfb5e77a6066cb4f0
Shalia Ramiah <shalia.r20@gmail.com>
2023-06-02 14:52:27 +0200

    oop

---
+++ Java/OOP/ooProgramming.java
@@ -0,0 +1,19 @@
+package Java.OOP;
+
+public class ooProgramming {
+
+    String colour = "blue";
+    String type = "ink";
+    int point = 7;
+
+    static boolean clicked = false; 
+
+    public static void click() {
+        clicked = true;
+    }
+
+    public static void unclick() {
+        clicked = false;
+    }
+    
+}
---
+++ Java/OOP/ooProgrammingMain.java
@@ -0,0 +1,13 @@
+package Java.OOP;
+
+public class ooProgrammingMain {
+    public static void main(String[] args) {
+        
+        ooProgramming o = new ooProgramming();
+
+        System.out.println(o.point);
+
+        o.click();
+        System.out.println(o.clicked);
+    }
+}
