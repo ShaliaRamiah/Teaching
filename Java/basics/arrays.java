f7e3ef2237983953900978e2a111f306aef5f6bc
Shalia Ramiah <shalia.r20@gmail.com>
2023-06-02 17:48:02 +0200

    arrays

---
+++ Java/basics/arrays.java
@@ -0,0 +1,17 @@
+package Java.basics;
+
+public class arrays {
+    public static void main(String[] args) {
+        System.out.println("Call of Duty Multiplayer");
+
+        String loot[] = {"Shotgun", "AK", "auto-sniper"};
+        String players[] = {"close range", "mid range", "long range"};
+
+        System.out.println("loot:");
+        System.out.println(loot[0]);
+        System.out.println(loot[1]);
+        System.out.println(loot[2]);
+
+    }
+    
+}
